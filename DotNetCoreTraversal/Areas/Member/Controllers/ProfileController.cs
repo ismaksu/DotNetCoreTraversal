82c847a338b881ed2d779ac5b25772622eba2657
ismaksu <mcpebagimli@gmail.com>
2022-12-30 20:26:53 +0300

    User Profile Page Added

---
+++ DotNetCoreTraversal/Areas/Member/Controllers/ProfileController.cs
@@ -0,0 +1,28 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreTraversal.Areas.Member.Controllers
+{
+    [Area("Member")]
+    [Route("Member/[controller]/[action]/{username?}")]
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public ProfileController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Users(string username)
+        {
+            var user = await _userManager.FindByNameAsync(username);
+            return View(user);
+        }
+    }
+}
