1721ccd93ef708e0333cc4f3b3eab81a735f93ef
ismaksu <mcpebagimli@gmail.com>
2023-03-04 11:37:17 +0300

    Changes on blog details section.

---
+++ DotNetCoreTraversal/ViewComponents/Destination/_GuideDetails.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreTraversal.ViewComponents.Destination
+{
+    public class _GuideDetails:ViewComponent
+    {
+        private readonly IGuideService _guideService;
+        public _GuideDetails(IGuideService guideService)
+        {
+            _guideService = guideService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _guideService.FindEntityByID(1);
+            return View(values);
+        }
+    }
+}
