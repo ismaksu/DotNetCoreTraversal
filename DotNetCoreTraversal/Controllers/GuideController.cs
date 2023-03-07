c1c710414eb2c2c4fbd171447d5b56dccd412493
ismaksu <mcpebagimli@gmail.com>
2023-03-07 17:50:23 +0300

    Guide page added.

---
+++ DotNetCoreTraversal/Controllers/GuideController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreTraversal.Controllers
+{
+    [AllowAnonymous]
+    public class GuideController : Controller
+    {
+        private readonly IGuideService _guideService;
+
+        public GuideController(IGuideService guideService)
+        {
+            _guideService = guideService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _guideService.ListEntities();
+            return View(values);
+        }
+    }
+}
