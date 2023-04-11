0555d814b2787e7cbfd34e01494c1c5fc0e0c326
ismaksu <mcpebagimli@gmail.com>
2023-04-11 16:26:23 +0300

    Role managing pages added on Admin.

---
+++ DotNetCoreTraversal/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,76 @@
+﻿using DotNetCoreTraversal.Areas.Admin.Models;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreTraversal.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]/{id?}")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _roleManager.Roles.ToList();
+            return View(values);
+        }
+
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel role)
+        {
+            AppRole appRole = new AppRole
+            {
+                Name = role.RoleName
+            };
+            var result = await _roleManager.CreateAsync(appRole);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(role);
+        }
+
+        public async Task<IActionResult> DeleteRole(int id)
+        {
+            var value = _roleManager.Roles.FirstOrDefault(x => x.Id == id);
+            await _roleManager.DeleteAsync(value);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult UpdateRole (int id)
+        {
+            var value = _roleManager.Roles.FirstOrDefault(x => x.Id == id);
+            UpdateRoleViewModel role = new UpdateRoleViewModel
+            {
+                RoleID = value.Id,
+                RoleName = value.Name
+            };
+            return View(role);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateRole(UpdateRoleViewModel role)
+        {
+            var value = _roleManager.Roles.FirstOrDefault(x => x.Id == role.RoleID);
+            value.Name = role.RoleName;
+            await _roleManager.UpdateAsync(value);
+            return RedirectToAction("Index");
+        }
+    }
+}
